e9e78a174c23394afca0ca746b41af8ba4b3c270
Alessandro Dovere <a.dovere@quadronica.com>
2022-02-11 17:29:54 +0100

    Initial Commit

---
+++ AppleSignInTest/AppleSignInTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSignInTestApp.swift
+//  AppleSignInTest
+//
+//  Created by Alessandro Dovere on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleSignInTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
